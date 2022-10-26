7165f7908212e020495492f6bf5bef4f19d501d9
JaxDoge <niftyjax@gmail.com>
2022-10-26 15:14:24 -0700

    start tuiter part

--- src/labs/a7/redux-examples/reducers/todos-reducer.js
+++ src/labs/a7/redux-examples/reducers/todos-reducer.js
@@ -2,12 +2,12 @@ import {createSlice} from "@reduxjs/toolkit";
 
 const initialTodos = [
     {
-        _id: "123",
+        _id: 123,
         do: "Accelerate the world's transition to sustainable energy",
         done: false
     },
     {
-        _id: "234",
+        _id: 34,
         do: "Reduce space transportation costs to become a spacefaring civilization",
         done: false
     },
@@ -16,6 +16,28 @@ const initialTodos = [
 const todosSlice = createSlice({
     name: 'todos',
     initialState: initialTodos,
+    reducers: {
+        addTodo(state, action) {
+            console.log(state);
+            state.push({
+                _id: (new Date()).getTime(),
+                do: action.payload.do,
+                done: false
+            });
+        },
+        deleteTodo(state, action) {
+            const index = action.payload
+            state.splice(index, 1)
+        },
+        todoDoneToggle(state, action) {
+            const target = state.find((todo, index) =>
+                index === action.payload)
+            target.done = !target.done
+        },
+    },
+
+
 });
 
+export const {addTodo, deleteTodo, todoDoneToggle} = todosSlice.actions
 export default todosSlice.reducer
