a0df54d919cf198cd6f18855a0f805b5ef728dba
JaxDoge <niftyjax@gmail.com>
2022-11-29 19:16:48 -0800

    implement dislike update

--- src/tuiter/tuits/TuitStats.js
+++ src/tuiter/tuits/TuitStats.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useDispatch} from "react-redux";
 import {updateTuitThunk} from "../../services/tuits-thunks";
 
+
 const TuitStats = ({post}) => {
     const dispatch = useDispatch();
 
@@ -24,6 +25,24 @@ const TuitStats = ({post}) => {
 
     }
 
+    const dislikeHandler = (thisPost) => {
+        if (!thisPost.disliked) {
+            dispatch(
+                updateTuitThunk({
+                    ...thisPost,
+                    dislikes: thisPost.dislikes + 1,
+                    disliked: true
+                }))
+        } else {
+            dispatch(
+                updateTuitThunk({
+                    ...thisPost,
+                    dislikes: thisPost.dislikes - 1,
+                    disliked: false
+                }))
+        }
+    }
+
     return(
         <div id="functional-panel" className="row mt-3">
             <div id="button-column" className="col-3 d-flex  align-content-center">
@@ -41,6 +60,16 @@ const TuitStats = ({post}) => {
                             : <i className="fa-regular fa-heart me-2"></i>
                     }
                 </button>{post.likes}
+            </div>
+            <div id="button-column" className="col-3 d-flex justify-content-center align-content-center">
+                <button type={"button"} className={"btn btn-link pt-0 ps-0 text-secondary"}
+                        onClick={() => dislikeHandler(post)}>
+                    {
+                        post.disliked
+                            ? <i className="fa-regular fa-thumbs-down text-dark me-2"></i>
+                            : <i className="fa-regular fa-thumbs-down me-2"></i>
+                    }
+                </button>{post.dislikes}
             </div>
             <div id="button-column" className="col-3 d-flex justify-content-center align-content-center">
                 <button type={"button"} className={"btn btn-link pt-0 ps-0 text-secondary"}><i className="fa-solid fa-arrow-up-from-bracket"></i></button>
